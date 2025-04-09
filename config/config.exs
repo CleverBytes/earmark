929ef7fa754eb93e52d8febb73cee6aa3ca8536d
Dforce <kcy493@gmail.com>
2025-04-10 02:44:56 +0500

    Update config.exs
    Fix Major Bugs

--- config/config.exs
+++ config/config.exs
@@ -6,7 +6,7 @@
 # Note this file is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project.
-
+# Note this file is loaded before any dependency and is restricted
 # Sample configuration:
 #
 #     config :my_dep,
